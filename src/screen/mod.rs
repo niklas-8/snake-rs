54eef71c1bdcd09f36d6c2f0acea02860730b854
niklas-8 <niklas.sauter@gmx.de>
2019-12-24 17:18:55 +0100

    rename WINDOW_SIZE in screen/mod.rs to SCREEN_SIZE

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -2,11 +2,11 @@ pub mod grid_position;
 
 pub const GRID_SIZE: (i16, i16) = (32, 20);
 pub const GRID_CELL_SIZE: (i16, i16) = (20, 20);
-const WINDOW_SIZE: (i16, i16) = (
+const SCREEN_SIZE: (i16, i16) = (
     GRID_SIZE.0 * GRID_CELL_SIZE.0,
     GRID_SIZE.1 * GRID_CELL_SIZE.1,
 );
 
 pub fn size() -> (f32, f32) {
-    (WINDOW_SIZE.0 as f32, WINDOW_SIZE.1 as f32)
+    (SCREEN_SIZE.0 as f32, SCREEN_SIZE.1 as f32)
 }
